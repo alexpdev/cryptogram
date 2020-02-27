3dd5aa749a1dc67b943e01d642e21a33a1335215
Alex <alexpdev@github.com>
2020-02-27 08:06:09 -0800

    finished tests for MapClass

--- tests/mapClass_test.py
+++ tests/mapClass_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from cryptogram import mapClass
-from cryptogram.mapClass import Map
+from cryptogram.mapClass import Map,InvalidChar
 from cryptogram.config import words
 
 
@@ -8,70 +8,59 @@ class MapTest(TestCase):
 
     def setUp(self):
         self.words = words
-        self.lst = []
-        for k,v in EXAMPLES.items():
-            kw = {
-                "key" : {},
-                "phrase": v["code"],
-                "final_key":v["final_key"],
-                "wordset":self.words,
-                "text":k
-            }
-            self.lst.append(kw)
+        self.kwargs = {
+            "wordset": self.words,
+            "phrase": "MYBBP",
+            "key": {"M":"H","B":"L"},
+            "mapp": (0,1,2,2,3),
+        }
+        self.mp = Map(**self.kwargs)
 
     def test_create_map(self):
-        for kwargs in self.lst:
-            mp = Map(**kwargs)
-            print(mp.phrase,mp.key)
-            self.assertEqual(mp.phrase,kwargs["phrase"])
-            self.assertEqual(mp.wordset,kwargs["wordset"])
-            self.assertIsInstance(mp,Map)
-            self.assertIsInstance(mp.phrase,str)
-            self.assertIsInstance(mp.wordset,set)
-        return
+        kwargs = self.kwargs
+        mp = Map(**kwargs)
+        self.assertEqual(mp.phrase,"MYBBP")
+        self.assertIsInstance(mp,Map)
+        self.assertEqual(mp.key,kwargs["key"])
+        self.assertIsInstance(kwargs["key"],dict)
+
+    def test_mapping(self):
+        args = ("HI !")
+        self.assertRaises(InvalidChar,self.mp.mapping,args)
+        self.assertEqual(self.mp.mapping("WHO"),(0,1,2))
+        self.assertEqual(self.mp.mapping("MAMMOTH"),(0,1,0,0,2,3,4))
+        self.assertIsInstance(self.mp.mapping("MAMMOTH"),tuple)
+        self.assertEqual(self.mp.mapping("THEY'RE"),(0,1,2,3,"'",4,2))
+
+    def test_gen_map(self):
+        txt = ("HELLO", "WORLD")
+        mapgen = self.mp.gen_map(txt)
+        self.assertIsInstance(next(mapgen),tuple)
+        print(type(mapgen))
+        self.assertEqual(next(mapgen),("WORLD",(0,1,2,3,4)))
+        del mapgen
+
+    def test_isMatch(self):
+        mp = self.mp
+        self.assertTrue(mp.isMatch("HELLO",mp.phrase))
+        self.assertFalse(mp.isMatch("BAT",mp.phrase))
+
+    def test_isDecrypt(self):
+        mp = self.mp
+        self.assertFalse(mp.isDecrypt(mp.phrase))
+        mp.key.update({"Y":"E","P":"O"})
+        self.assertTrue(mp.isDecrypt(mp.phrase))
+        mp.key.update({"Y":"E","P":"O","B":"L"})
 
 
-EXAMPLES = {
-    "THAT'S": {
-        'code': "LWBH'F",
-        'final_key': {
-            'L': 'T',
-            'W': 'H',
-            'B': 'A',
-            'H': 'T',
-            'F': 'S'
-            }
-        },
-    'HELLO WORLD': {
-        'code': 'ZTBYL RWWON',
-        'final_key': {
-            'Z': 'H',
-            'T': 'E',
-            'B': 'L',
-            'Y': 'L',
-            'L': 'O',
-            'R': 'W',
-            'W': 'O',
-            'N': 'D'
-            }
-        },
-    'YOU!': {
-        'code': 'SFN!',
-        'final_key': {
-            'S': 'Y',
-            'F': 'O',
-            'N': 'U'
-            }
-        },
-    'YOU! FAIL!': {
-        'code': 'TQF! UKXI!',
-        'final_key': {
-            'T': 'Y',
-            'Q': 'O',
-            'F': 'U',
-            'K': 'A',
-            'X': 'I',
-            'I': 'L'
-            }
-        }
-}
+    def test_addkeys(self):
+        mp = self.mp
+        mp.addKeys(mp.phrase,"HELLO")
+        self.assertEqual(mp.key,{"M":"H","Y":"E","P":"O","B":"L"})
+        print(mp.key)
+
+    def test_removeKeys(self):
+        mp = self.mp
+        mp.removeKeys(("B","M"),mp.phrase)
+        self.assertNotIn("B",mp.key)
+        self.assertNotIn("M",mp.key)
